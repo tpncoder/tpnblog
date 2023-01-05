9b314831a7249fd383f54b4804243dcd85a64f9b
tpncoder <tejasprotim@gmail.com>
2023-01-05 19:09:20 +0530

    fix error

--- src/routes/[slug]/+page.js
+++ src/routes/[slug]/+page.js
@@ -1,5 +1,3 @@
-export const prerender = true;
-
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch, params }) {
 	const res = await fetch(`../../articles/${params.slug}.md`);
