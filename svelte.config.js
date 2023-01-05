05b858d21dc37b82313d6dcdea8ec4bf8fbd160a
tpncoder <tejasprotim@gmail.com>
2023-01-05 19:07:30 +0530

    mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from 'mdsvex'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +8,12 @@ const config = {
 		adapter: adapter()
 	},
 
+	extensions: ['.svelte', '.md'],
+
 	preprocess: [
+		mdsvex({
+			extensions: ['.md']
+		}),	
 		preprocess({
 			postcss: true
 		})
