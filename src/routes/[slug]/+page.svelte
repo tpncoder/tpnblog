c13fc3fb5b6e699b0746108ab5e92cb41ac7aac3
tpncoder <tejasprotim@gmail.com>
2023-02-11 08:26:50 +0530

    .

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -3,11 +3,9 @@
 	import { onMount } from 'svelte';
 		
 	let slug = $page.params.slug;
-
-	import { marked } from 'marked'
 </script>
 
 <h1 class='text-center text-6xl text-black dark:text-white mt-5'>{$page.params.slug}</h1>
 <p class='relative left-[25%] text-xl mt-7 w-2/4 text-justif text-black dark:text-white leading-10 mb-6'>
-{@html marked($page.data.item)}
+{@html $page.data.item}
 </p>
