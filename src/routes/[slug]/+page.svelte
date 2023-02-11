09446f8e2f83fe43a12938bdb328276b7d89b1bf
tpncoder <tejasprotim@gmail.com>
2023-02-11 08:23:03 +0530

    add welcome articles

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -3,7 +3,11 @@
 	import { onMount } from 'svelte';
 		
 	let slug = $page.params.slug;
+
+	import { marked } from 'marked'
 </script>
 
 <h1 class='text-center text-6xl text-black dark:text-white mt-5'>{$page.params.slug}</h1>
-<p class='relative left-[25%] text-xl mt-7 w-2/4 text-justif text-black dark:text-white leading-10 mb-6'>{@html $page.data.item}</p>
+<p class='relative left-[25%] text-xl mt-7 w-2/4 text-justif text-black dark:text-white leading-10 mb-6'>
+{@html marked($page.data.item)}
+</p>
