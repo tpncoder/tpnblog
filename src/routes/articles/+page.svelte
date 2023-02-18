84f1016b371fee8d962d30b56812d50bc0da4af3
tpncoder <tejasprotim@gmail.com>
2023-02-18 09:43:31 +0530

    .

--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -1,11 +1,11 @@
 <script>
 	import { Card } from "flowbite-svelte";
 
-	const imageModules = import.meta.glob("../../articles/*.md");
+	const imageModules = import.meta.glob("../../../static/articles/*.md");
 </script>
 
 {#each Object.entries(imageModules) as [title, paragraph]}
 	<Card class='flex m-auto mb-5 mt-5'>	
-		<a href={title.slice(15).replace('.md', '').replace(' ', '-')}>{title.slice(15).replace('.md', '')}</a>
+		<a href={title.slice(25).replace('.md', '').replace(' ', '-')}>{title.slice(25).replace('.md', '')}</a>
 	</Card>	
 {/each}
